edeaafd96d912ca21da3e00b8d4cb5ca15872eb8
Isaiah LaDuke <iladuke@atsapllc.com>
2024-04-17 11:19:46 -0400

    feat: I added my finishing statement to the bottom of the file to explain why I did what I did.

--- TwoSumII.js
+++ TwoSumII.js
@@ -18,3 +18,10 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+// Finishing Statement: After completing this problem, I understand two important things, the importance of a sorted array
+// and the importance of distinguishing between weakly and stricly changing values in an array. The sorted array is important
+// in this case because the action of "moving" left or right based on the result of sum wouldn't be logical without the array being
+// sorted. For the weakly and strictly changing values, this affects whether there are duplicates allowed within the array, this can
+// affect the logic of our algorithm and could cause issues if you have a repeat of a sum value. In the case of how I created my function,
+// it won't be a problem, it would just be slower if the array has duplicates to be searched through.
